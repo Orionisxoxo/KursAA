6e330b4ebeae7c1b9a79a1e0369661f750f954e0
Iwona Lachowska <iwona.lachowska@upaid.pl>
2021-01-03 17:09:15 +0100

    Homework module 3

--- app/src/main/java/com/example/kursaa/core/di/KoinInjector.kt
+++ app/src/main/java/com/example/kursaa/core/di/KoinInjector.kt
@@ -3,5 +3,7 @@ package com.example.kursaa.core.di
 import org.koin.core.module.Module
 
 val koinInjector: List<Module> = listOf(
-    networkModule
+    networkModule,
+    appModule,
+    databaseModule
 )
